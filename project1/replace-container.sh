19bdba615b40f7ef448bc91257c6b6e9a1a3fb38
Ziwei112 <96652367+Ziwei112@users.noreply.github.com>
2022-01-31 21:27:01 +0800

    Delete project1 directory

--- project1/replace-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop con
-docker rm con
-docker pull ziwei0/awsdevops
-docker run -d -p 8080:8080 --name con ziwei0/awsdevops
--- project1/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>ABCDEFG</h2>
-</body>
-</html>
